10f86d05e0da5e3b27f55f76f52dfabb7cb1bb9a
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-17 20:40:22 -0400

    refactor PostContainer to use a class

--- instagram/instagram/src/components/PostComponent/PostContainer.js
+++ instagram/instagram/src/components/PostComponent/PostContainer.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import CommentSection from '../CommentComponent/CommentSection'
-import './PostContainer.css'
+import React from "react";
+import CommentSection from "../CommentComponent/CommentSection";
+import "./PostContainer.css";
 
-
-function PostContainer (props) {
-    const {posts} = props
+class PostContainer extends React.Component {
+  render() {
     return (
-        <div>
-            {posts.map((post, index) => (
-               <div className="card" key={index}>
-                   <div className="card-profile">
-                        <img src={post.thumbnailUrl} alt={post.username} />
-                        {post.username}
-                   </div>
-                   <div className="post-img">
-                        <img src={post.imageUrl} alt={post.username} />
-                   </div>
-                    <div className="card-likes">
-                        <i className="far fa-heart"></i>
-                        <i className="far fa-comment"></i>
-                        <p>{post.likes} likes </p>
-                    </div>
-                     <div className="comments">
-                         <CommentSection comments = {post.comments}/>
-                    </div>
-                </div>
-                
-            )
-            )}
-        </div>
-    )
+      <div>
+        {this.props.posts.map((post, index) => (
+          <div className="card" key={index}>
+            <div className="card-profile">
+              <img src={post.thumbnailUrl} alt={post.username} />
+              {post.username}
+            </div>
+            <div className="post-img">
+              <img src={post.imageUrl} alt={post.username} />
+            </div>
+            <div className="card-likes">
+              <i className="far fa-heart" />
+              <i className="far fa-comment" />
+              <p>{post.likes} likes </p>
+            </div>
+            <div className="comments">
+              <CommentSection comments={post.comments} />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default PostContainer
+export default PostContainer;
