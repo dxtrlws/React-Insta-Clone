4d1fd3211c7e08270f58ca9046c5dea18089351b
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-19 21:06:30 -0400

    Removed unused constructor in App js

--- instagram/instagram/src/App.js
+++ instagram/instagram/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import dummyData from "./dummy-data";
 import "./App.css";
 
 import PostsPage from "./components/PostComponent/PostsPage";
@@ -10,15 +9,6 @@ import Login from './components/Login/Login'
 const ComponentFromWithAuthenticate = withAuthenticate(PostsPage,Login)
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state ={
-      isLoggedIn: false
-    }
-  }
-
-
- 
   render() {
     return (
       <div className="container">
