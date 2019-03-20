81ca5b2b87302c31a5a5159897aa1b3bf815f0a5
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-19 20:54:20 -0400

    commented code

--- instagram/instagram/src/components/Login/Login.js
+++ instagram/instagram/src/components/Login/Login.js
@@ -9,11 +9,11 @@ class Login extends React.Component {
       password: ""
     };
   }
-
+  // handles storing values of the text boxes into state
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-
+  // Sets username to local storage and forces a page refresh
   formSubmit = e => {
     e.preventDefault();
     localStorage.setItem("username", this.state.username);
@@ -22,35 +22,39 @@ class Login extends React.Component {
 
   render() {
     return (
-        <div className="login-pg">
-            <div className="IG-image">
-                <img src="/images/InstagramMock.png" alt="logo" />
-            </div>
-      <div className="login-box">
-        <img className="login-logo" alt="logo" src="/images/instagram.png" />
-        <form className="login-form" onSubmit={this.formSubmit}>
-          <input
-            onChange={this.handleChange}
-            name="username"
-            type="text"
-            placeholder="User Name"
-            value={this.state.username}
-          />
-          <input
-            onChange={this.handleChange}
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-          />
-          <button className="btn btn-primary" type="submit">
-            Login
-          </button>
-        </form>
-        <p>Get the App</p>
-        <img className="app-store" src="/images/appStores.png" alt="App Store" />
-      </div>
+      <div className="login-pg">
+        <div className="IG-image">
+          <img src="/images/InstagramMock.png" alt="logo" />
         </div>
+        <div className="login-box">
+          <img className="login-logo" alt="logo" src="/images/instagram.png" />
+          <form className="login-form" onSubmit={this.formSubmit}>
+            <input
+              onChange={this.handleChange}
+              name="username"
+              type="text"
+              placeholder="User Name"
+              value={this.state.username}
+            />
+            <input
+              onChange={this.handleChange}
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+            />
+            <button className="btn btn-primary" type="submit">
+              Login
+            </button>
+          </form>
+          <p>Get the app</p>
+          <img
+            className="app-store"
+            src="/images/appStores.png"
+            alt="App Store"
+          />
+        </div>
+      </div>
     );
   }
 }
